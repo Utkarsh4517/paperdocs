57d564179f07a7e709ec32dde06ac7370b9e6024
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-03-27 18:11:46 +0530

    connected with mongo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const DB = require("config/dbConfig.js")
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+mongoose.connect(DB).then(() => console.log('Database connected')).catch((e) => console.log(e));
 
 app.listen(PORT, "0.0.0.0", () => console.log("Server connected"));

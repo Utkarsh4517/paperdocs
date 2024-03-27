dd496a59947cb1e269defef56326b768bd922053
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-03-27 15:37:20 +0530

    server connected

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => console.log("Server connected"));
